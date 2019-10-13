39a2fe5690e25e375e3c6d900d2facf88a82fded
Rodrigo Boniatti <boniatti.rodrigo@gmail.com>
2019-10-12 22:44:13 -0300

    Create fight generator

--- src/battle_of_the_bands/core.clj
+++ src/battle_of_the_bands/core.clj
@@ -1,7 +1,59 @@
 (ns battle-of-the-bands.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
+(def bands [
+  "Iron Maiden"
+  "Metallica"
+  "Foo Fighters"
+  "Queens of the stone age"
+  "Rage Against the Machine"
+  "Audioslave"
+  "Nirvana"
+  "The Offspring"
+  "Led Zeppelin"
+  "Queen"
+  "Arctic Monkeys"
+  "Deep Purple"
+  "ACDC"
+  "Ramones"
+  "Kiss"
+  "The Doors"
+])
+
+(defn clashes-generator
+  [bands]
+  (let [
+        bands-shuffled (partition (/ (count bands) 2) (shuffle bands))
+        first-bands (first bands-shuffled)
+        last-bands (last bands-shuffled)
+        ]
+        (zipmap first-bands last-bands)
+  ))
+
+(defn select-band
+  [bands-selected band-fight]
+    (let [band-one (first band-fight)
+          band-two (last band-fight)
+          ]
+      (println "Which one is better?")
+    (println (str "1 - " band-one))
+    (println (str "2 - " band-two))
+    (def option (read-line))
+    (if (= (str option) "1") (cons band-one bands-selected) (cons band-two bands-selected))))
+
+(defn fight-generator
+  [b]
+  (if (= (count b) 1)
+    (println (str "THE CHAMPION IS: " (first b)))
+    (do
+      (println "---- NEW ROUND ---")
+      (recur (reduce select-band [] (clashes-generator b)))
+      )
+  ))
+
+      (defn -main
   [& args]
-  (println "Hello, World!"))
+  (println "The fights will start. Are you ready?")
+  (println "Go!")
+  (fight-generator bands)
+  )
